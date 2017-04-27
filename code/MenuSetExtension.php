7a7eb82487560c686a056bd26c690e96c5ca9984
Ben Manu <bmanu10@gmail.com>
2017-04-27 15:04:12 +1200

    Adding subsite defaults.

--- code/MenuSetExtension.php
+++ code/MenuSetExtension.php
@@ -6,6 +6,8 @@ use DataExtension;
 use FieldList;
 use HiddenField;
 use Subsite;
+use MenuSet;
+use DB;
 
 class MenuSetExtension extends DataExtension
 {
@@ -24,4 +26,30 @@ class MenuSetExtension extends DataExtension
         $this->owner->SubsiteID = Subsite::currentSubsiteID();
     }
 
+    public function requireDefaultRecords()
+    {
+        $subsites = Subsite::all_sites();
+        $defaultSetNames = $this->owner->config()->get('default_sets') ?: array();
+        Subsite::$use_session_subsiteid = true;
+
+        $subsites->each(function ($subsite) use ($defaultSetNames) {
+            Subsite::changeSubsite($subsite->ID);
+
+            foreach ($defaultSetNames as $name) {
+                $existingRecord = MenuSet::get()->filter([
+                    'Name' => $name,
+                    'SubsiteID' => $subsite->ID,
+                ])->first();
+
+                if (!$existingRecord) {
+                    $set = new MenuSet();
+                    $set->Name = $name;
+                    $set->write();
+
+                    DB::alteration_message("MenuSet '$name' created for Subsite", 'created');
+                }
+            }
+        });
+        Subsite::$use_session_subsiteid = false;
+    }
 }
