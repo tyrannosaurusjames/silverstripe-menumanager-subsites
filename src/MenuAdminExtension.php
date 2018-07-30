be5f0970f55234fc9eca9876f37a0fa42c248b44
Adrian Humphreys <adrhumphreys@gmail.com>
2018-07-31 09:14:24 +1200

    update the admin field to filter the list

--- src/MenuAdminExtension.php
+++ src/MenuAdminExtension.php
@@ -4,19 +4,33 @@ namespace Guttmann\SilverStripe;
 
 use Heyday\MenuManager\MenuSet;
 use SilverStripe\Core\Extension;
+use SilverStripe\Forms\Form;
 use SilverStripe\Subsites\State\SubsiteState;
 
 class MenuAdminExtension extends Extension
 {
-    public function updateEditForm($form)
+    public function updateEditForm(Form $form)
     {
-        $gridField = $form->Fields()->fieldByName(MenuSet::class);
-        if($gridField) {
+        if ($this->owner->modelClass === MenuSet::class) {
+            $gridFieldName = $this->extSanitiseClassName($this->owner->modelClass);
+            $gridField = $form->Fields()->fieldByName($gridFieldName);
             $list = $gridField->getList();
-            $filteredList = $list->filter(array(
-                'SubsiteID' => SubsiteState::singleton()->getSubsiteId()
-            ));
+            $filteredList = $list->filter(
+                [
+                    'SubsiteID' => SubsiteState::singleton()->getSubsiteId()
+                ]
+            );
             $gridField->setList($filteredList);
         }
+
+        return $form;
+    }
+
+    /*
+     * This is required as we can't call the model admins sanitiseClassName
+     */
+    public function extSanitiseClassName($class)
+    {
+        str_replace('\\', '-', $class);
     }
 }
